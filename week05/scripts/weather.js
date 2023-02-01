1834fab66ad98da687c2637e2924915ba6d3cb7f
kellyannclark <thefaithseed@gmail.com>
2023-01-31 17:18:44 -0700

    changees

---
+++ week05/scripts/weather.js
@@ -0,0 +1,43 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+
+// Declare a const variable name "url" and assign it a valid URL string
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Use the Current Weather API and start a query string
+const apiKey = '15a1ab955380b7a83ec1c9828e8bc826';
+const city = 'Trier';
+const units = 'imperial';
+const queryString = `?q=${city}&units=${units}&appid=${apiKey}`;
+
+const fullURL = url + queryString;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayData(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}10d.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+
