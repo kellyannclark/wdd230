640856c0ad33a0f559d5f56275a68fc91f8edfa4
kellyannclark <thefaithseed@gmail.com>
2023-02-08 20:36:00 -0700

    changes

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,43 +1,37 @@
 const memberList = document.getElementById("memberList");
 
 fetch("data/members.json")
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
   .then(data => {
     let companies = data.companies;
     for (let company of companies) {
       let li = document.createElement("li");
-
-      let h2 = document.createElement("h2");
-      h2.textContent = company.name;
-      li.appendChild(h2);
-
-      let address = document.createElement("p");
-      address.textContent = `Address: ${company.address}`;
-      li.appendChild(address);
-
-      let phoneNumber = document.createElement("p");
-      phoneNumber.textContent = `Phone Number: ${company.phonenumber}`;
-      li.appendChild(phoneNumber);
-
-      let website = document.createElement("p");
-      let websiteLink = document.createElement("a");
-      websiteLink.href = company.websiteurl;
-      websiteLink.textContent = company.websiteurl;
-      website.appendChild(websiteLink);
-      li.appendChild(website);
-
-      let img = document.createElement("img");
-      img.src = company.logourl;
-      img.alt = company.name;
-      img.width = 150;
-      img.height = 150;
-      li.appendChild(img);
-
-      let membershipLevel = document.createElement("p");
-      membershipLevel.textContent = `Membership Level: ${company.membershiplevel}`;
-      li.appendChild(membershipLevel);
-
+      li.innerHTML = `
+        <h2>${company.name}</h2>
+        <p>Address: ${company.address}</p>
+        <p>Phone Number: ${company.phonenumber}</p>
+        <p>Website URL: <a href="${company.websiteurl}">${company.websiteurl}</a></p>
+        <p><img src="${company.logourl}" alt=${company.name}></p>
+        <p>Membership Level: ${company.membershiplevel}</p>
+      `;
       memberList.appendChild(li);
     }
-  });
+  })
+  .catch(error => console.error(error));
 
+const membersContainer = document.querySelector("#members");
+
+document.querySelector("#grid-view").addEventListener("click", () => {
+  membersContainer.classList.remove("list-view");
+  membersContainer.classList.add("grid-view");
+});
+
+document.querySelector("#list-view").addEventListener("click", () => {
+  membersContainer.classList.remove("grid-view");
+  membersContainer.classList.add("list-view");
+});
